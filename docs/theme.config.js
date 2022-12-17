0b94cc1212fc664038f0a8e4c9ac2b68df48907b
winoffrg <61311751+WINOFFRG@users.noreply.github.com>
2022-12-18 01:31:17 +0530

    Added About US Page

--- docs/theme.config.js
+++ docs/theme.config.js
@@ -1,7 +1,14 @@
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <h2>Limetree Player</h2>,
   project: {
     link: "https://github.com/winoffrg/limetree",
   },
-  // ...
+  banner: {
+    key: "contribute",
+    text: (
+      <a href="https://github.com/winoffrg/limetree" target="_blank">
+        🍀 Limetree is a project in active development, It aims to be a fully fledged Open Source library. Contributors are Welcomed!
+      </a>
+    ),
+  }
 };
