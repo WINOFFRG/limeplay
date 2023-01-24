62805ff5486227a7195027b4cf340aad2c72036a
winoffrg <rohangupta1528@gmail.com>
2023-01-24 17:49:08 +0530

    Fixed Structure, Files

---
+++ docs/.contentlayer/.cache/v0.2.9/compiled-contentlayer-config-FIITXVPB.mjs
@@ -0,0 +1,17 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+var Guide = defineDocumentType(() => ({
+  name: "Guide",
+  contentType: "mdx",
+  filePathPattern: "/*.mdx",
+  fields: {}
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "docs",
+  documentTypes: [Guide]
+});
+export {
+  Guide,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-FIITXVPB.mjs.map
---
+++ docs/.contentlayer/.cache/v0.2.9/compiled-contentlayer-config-LCYX56SE.mjs
@@ -0,0 +1,108 @@
+// contentlayer.config.ts
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+var Guide = defineDocumentType(() => ({
+  name: "Guide",
+  contentType: "mdx",
+  filePathPattern: "**/*.mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true
+    },
+    description: {
+      type: "string"
+    },
+    props: {
+      type: "list",
+      required: true,
+      of: {
+        type: "string"
+      }
+    },
+    import: {
+      type: "string",
+      required: true
+    },
+    docs: {
+      type: "string",
+      required: true
+    },
+    source: {
+      type: "string",
+      required: true
+    },
+    package: {
+      type: "string",
+      required: true
+    },
+    installation: {
+      type: "string",
+      required: true
+    },
+    pageTitle: {
+      type: "string",
+      required: true
+    },
+    license: {
+      type: "string",
+      required: true
+    },
+    styles: {
+      type: "list",
+      required: true,
+      of: {
+        type: "string"
+      }
+    },
+    group: {
+      type: "string"
+    },
+    order: {
+      type: "number",
+      required: true
+    },
+    slug: {
+      type: "string",
+      required: true
+    },
+    category: {
+      type: "string",
+      required: true
+    },
+    release: {
+      type: "string",
+      required: true
+    },
+    date: {
+      type: "string",
+      required: true
+    },
+    search: {
+      type: "string"
+    },
+    error: {
+      type: "string"
+    },
+    componentPrefix: {
+      type: "string"
+    },
+    hideToc: {
+      type: "boolean"
+    },
+    polymorphic: {
+      type: "boolean"
+    },
+    hidden: {
+      type: "boolean"
+    }
+  }
+}));
+var contentlayer_config_default = makeSource({
+  contentDirPath: "docs",
+  documentTypes: [Guide]
+});
+export {
+  Guide,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-LCYX56SE.mjs.map
