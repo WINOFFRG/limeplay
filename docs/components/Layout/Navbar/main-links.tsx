838902a215c96c3a7509a90d7ea00136daa78c6a
winoffrg <rohangupta1528@gmail.com>
2023-01-25 03:44:12 +0530

    Fixing Build Scripts for Docs

--- docs/components/Layout/Navbar/main-links.tsx
+++ docs/components/Layout/Navbar/main-links.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { DEFAULT_THEME } from '@mantine/core';
-import { IconCode, IconStar, IconRocket } from '@tabler/icons';
+import { IconCode, IconStar, IconRocket, IconSearch } from '@tabler/icons';
 
-export default [];
+export default [
+    {
+        to: '/pages/about',
+        label: 'About Project',
+        color: DEFAULT_THEME.colors.yellow[5],
+        icon: IconSearch,
+    },
+];
