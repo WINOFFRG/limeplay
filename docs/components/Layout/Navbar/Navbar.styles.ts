23f268b1205707ce9004d9d7be0bfb77dbdb5d0b
winoffrg <rohangupta1528@gmail.com>
2023-01-25 03:28:32 +0530

    Final Styling for Navbar

--- docs/components/Layout/Navbar/Navbar.styles.ts
+++ docs/components/Layout/Navbar/Navbar.styles.ts
@@ -6,45 +6,48 @@ export const NAVBAR_WIDTH = 260;
 export const NAVBAR_BREAKPOINT = 760;
 
 export default createStyles((theme: MantineTheme) => ({
-  navbar: {
-    boxSizing: 'border-box',
-    height: '100vh',
-    borderRight: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[2]
-    }`,
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
-    position: 'fixed',
-    zIndex: 5,
-    top: 0,
-    bottom: 0,
-    left: 0,
-    width: NAVBAR_WIDTH,
+    navbar: {
+        boxSizing: 'border-box',
+        height: '100vh',
+        borderRight: `1px solid ${
+            theme.colorScheme === 'dark'
+                ? theme.colors.dark[7]
+                : theme.colors.gray[2]
+        }`,
+        backgroundColor:
+            theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+        position: 'fixed',
+        zIndex: 5,
+        top: 0,
+        bottom: 0,
+        left: 0,
+        width: NAVBAR_WIDTH,
 
-    [`@media (max-width: ${NAVBAR_BREAKPOINT}px)`]: {
-      display: 'none',
+        [`@media (max-width: ${NAVBAR_BREAKPOINT}px)`]: {
+            display: 'none',
+        },
     },
-  },
 
-  opened: {
-    [`@media (max-width: ${NAVBAR_BREAKPOINT}px)`]: {
-      display: 'block',
-      width: '100%',
-      right: 0,
+    opened: {
+        [`@media (max-width: ${NAVBAR_BREAKPOINT}px)`]: {
+            display: 'block',
+            width: '100%',
+            right: 0,
+        },
     },
-  },
 
-  body: {
-    paddingRight: theme.spacing.md,
-    paddingBottom: theme.spacing.xl * 2,
-    paddingLeft: theme.spacing.md,
-    paddingTop: HEADER_HEIGHT + theme.spacing.md,
+    body: {
+        paddingRight: theme.spacing.md,
+        paddingBottom: theme.spacing.xl * 2,
+        paddingLeft: theme.spacing.md,
+        paddingTop: HEADER_HEIGHT + theme.spacing.md,
 
-    [`@media (max-width: ${NAVBAR_BREAKPOINT}px)`]: {
-      paddingBottom: 120,
+        [`@media (max-width: ${NAVBAR_BREAKPOINT}px)`]: {
+            paddingBottom: 120,
+        },
     },
-  },
 
-  docs: {
-    marginTop: theme.spacing.xl,
-  },
+    docs: {
+        marginTop: theme.spacing.xl,
+    },
 }));
--- docs/components/Layout/Navbar/main-links.tsx
+++ docs/components/Layout/Navbar/main-links.tsx
@@ -2,23 +2,4 @@ import React from 'react';
 import { DEFAULT_THEME } from '@mantine/core';
 import { IconCode, IconStar, IconRocket } from '@tabler/icons';
 
-export default [
-    {
-        to: '/pages/getting-started/',
-        label: 'Getting started',
-        color: DEFAULT_THEME.colors.blue[5],
-        icon: IconRocket,
-    },
-    {
-        to: '/pages/basics/',
-        label: 'Learn the basics',
-        color: DEFAULT_THEME.colors.violet[5],
-        icon: IconStar,
-    },
-    {
-        to: '/pages/contributing/',
-        label: 'Contribute',
-        color: '#000',
-        icon: IconCode,
-    },
-];
+export default [];
