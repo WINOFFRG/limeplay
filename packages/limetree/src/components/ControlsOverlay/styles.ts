45c7f06288020803f1d7c2c864e4aa732708a720
winoffrg <rohangupta1528@gmail.com>
2023-02-05 17:21:32 +0530

    Added Playback Seek Bar with New Project Sturcture

--- packages/limetree/src/components/ControlsOverlay/styles.ts
+++ packages/limetree/src/components/ControlsOverlay/styles.ts
@@ -23,7 +23,7 @@ const useStyles = createStyles((theme) => ({
         padding: `0 ${theme.spacing.xl}px`,
         paddingTop: theme.spacing.md,
         flex: `0 1 auto`,
-        backgroundImage: `linear-gradient(180deg,rgba(0,0,0,.0001),rgba(0,0,0,.0156863) 8.62%,rgba(0,0,0,.0509804) 16.56%,rgba(0,0,0,.113725) 23.93%,rgba(0,0,0,.188235) 30.85%,rgba(0,0,0,.278431) 37.42%,rgba(0,0,0,.372549) 43.77%,rgba(0,0,0,.47451) 50%,rgba(0,0,0,.576471) 56.23%,rgba(0,0,0,.67451) 62.58%,rgba(0,0,0,.760784) 69.15%,rgba(0,0,0,.839216) 76.07%,rgba(0,0,0,.898039) 83.44%,rgba(0,0,0,.937255) 91.38%,rgba(0,0,0,.94902))`,
+        // backgroundImage: `linear-gradient(180deg,rgba(0,0,0,.0001),rgba(0,0,0,.0156863) 8.62%,rgba(0,0,0,.0509804) 16.56%,rgba(0,0,0,.113725) 23.93%,rgba(0,0,0,.188235) 30.85%,rgba(0,0,0,.278431) 37.42%,rgba(0,0,0,.372549) 43.77%,rgba(0,0,0,.47451) 50%,rgba(0,0,0,.576471) 56.23%,rgba(0,0,0,.67451) 62.58%,rgba(0,0,0,.760784) 69.15%,rgba(0,0,0,.839216) 76.07%,rgba(0,0,0,.898039) 83.44%,rgba(0,0,0,.937255) 91.38%,rgba(0,0,0,.94902))`,
     },
 
     topRightSection: {
@@ -38,6 +38,13 @@ const useStyles = createStyles((theme) => ({
         flex: 1,
         flexDirection: 'column',
         alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    controlsBottomPanelWrapper: {
+        display: 'flex',
+        flexDirection: 'column',
+        padding: `0 ${theme.spacing.xl}px`,
     },
 
     controlsBottomPanel: {
@@ -48,11 +55,35 @@ const useStyles = createStyles((theme) => ({
         justifyContent: 'space-between',
         height: 'auto',
         width: '100%',
-        padding: `0 ${theme.spacing.xl}px`,
         paddingBottom: theme.spacing.md,
         flex: `0 1 auto`,
         backgroundImage: `linear-gradient(180deg,rgba(0,0,0,.0001),rgba(0,0,0,.0156863) 8.62%,rgba(0,0,0,.0509804) 16.56%,rgba(0,0,0,.113725) 23.93%,rgba(0,0,0,.188235) 30.85%,rgba(0,0,0,.278431) 37.42%,rgba(0,0,0,.372549) 43.77%,rgba(0,0,0,.47451) 50%,rgba(0,0,0,.576471) 56.23%,rgba(0,0,0,.67451) 62.58%,rgba(0,0,0,.760784) 69.15%,rgba(0,0,0,.839216) 76.07%,rgba(0,0,0,.898039) 83.44%,rgba(0,0,0,.937255) 91.38%,rgba(0,0,0,.94902))`,
         // marginTop: '-400px',
+    },
+
+    centrePlaybackIcon: {
+        background:
+            'radial-gradient(rgba(0,0,0,0.2), rgba(0,0,0,0.6) 77.5%, transparent 97.5%)',
+        borderRadius: '50%',
+        padding: '14px',
+        margin: '0 auto',
+        animation: 'zoom-and-fade-out 500ms ease-out',
+        display: 'none',
+    },
+
+    iconAnimation: {
+        '@keyframes zoom-and-fade-out': {
+            '0%': {
+                transform: 'scale(0)',
+                opacity: 1,
+                display: 'block',
+            },
+            '100%': {
+                transform: 'scale(1.5)',
+                opacity: 0,
+                display: 'none',
+            },
+        },
     },
 }));
 
--- packages/limetree/vite.config.ts
+++ packages/limetree/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react(), splitVendorChunkPlugin()],
+    resolve: {
+        alias: {
+            'shaka-player': 'shaka-player/dist/shaka-player.compiled',
+        },
+    },
+    //   optimizeDeps: {
+    //     include: ['shaka-player'],
+    //   },
+    //   build: {
+    //     commonjsOptions: {
+    //       include: [/shaka-player/, /node_modules/],
+    //     },
+    //   },
+});
