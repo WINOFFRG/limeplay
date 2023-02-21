dad6e77b9863b9e671e92c98f048137780473fc0
winoffrg <rohangupta1528@gmail.com>
2023-02-22 00:29:23 +0530

    Added Missing Packages

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,5 +1,7 @@
 const { withContentlayer } = require('next-contentlayer');
 
-const nextConfig = {};
+const nextConfig = {
+    ignoreBuildErrors: true,
+};
 
 module.exports = withContentlayer(nextConfig);
