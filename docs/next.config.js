eddd8478b401aa77e815e70fb3c3140a3299b7a3
winoffrg <rohangupta1528@gmail.com>
2023-02-26 00:56:19 +0530

    Next Config Fix

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,4 +1,4 @@
-const { withContentlayer } = require('next-contentlayer');
+import withContentlayer from 'next-contentlayer';
 
 const nextConfig = {
 	typescript: {
@@ -9,4 +9,4 @@ const nextConfig = {
 	},
 };
 
-module.exports = withContentlayer(nextConfig);
+export default withContentlayer(nextConfig);
