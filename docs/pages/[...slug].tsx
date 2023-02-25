63d960392e9f5e459d9c3d4e1dfc25b62da2db13
winoffrg <rohangupta1528@gmail.com>
2023-02-26 02:04:52 +0530

    Testing Diff and Added Home Page

--- docs/pages/[...slug].tsx
+++ docs/pages/[...slug].tsx
@@ -1,71 +1,68 @@
 import React from 'react';
-import { MdxPage } from '../components/MdxPage/MdxPage';
 import { allDocuments, type DocumentTypes } from 'contentlayer/generated';
+import { GetStaticProps } from 'next';
+import { MdxPage } from '../components/MdxPage/MdxPage';
 import Layout from '@/components/Layout/Layout';
 import { MdxErrorPage } from '@/components/MdxPage/MdxErrorPage/MdxErrorPage';
 import SEO from '@/components/seo';
-import { GetStaticProps } from 'next';
 
 export default function DocPage({
-    mdx,
-    error,
+	mdx,
+	error,
 }: {
-    mdx: DocumentTypes;
-    error?: boolean;
+	mdx: DocumentTypes;
+	error?: boolean;
 }) {
-    if (!mdx || error) {
-        return (
-            <>
-                <Layout>
-                    <MdxErrorPage />
-                </Layout>
-            </>
-        );
-    }
+	if (!mdx || error) {
+		return (
+			<Layout>
+				<MdxErrorPage />
+			</Layout>
+		);
+	}
 
-    return (
-        <>
-            <SEO title={mdx.title} description={mdx.description} />
-            <Layout>
-                <article>
-                    <MdxPage mdx={mdx} />
-                </article>
-            </Layout>
-        </>
-    );
+	return (
+		<>
+			<SEO title={mdx.title} description={mdx.description} />
+			<Layout>
+				<article>
+					<MdxPage mdx={mdx} />
+				</article>
+			</Layout>
+		</>
+	);
 }
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-    const document = allDocuments.find((post) => {
-        const pathAsked =
-            params?.slug && Array.isArray(params.slug)
-                ? '/' + params.slug.join('/')
-                : params?.slug;
-        const pathFound = post.slug;
+	const document = allDocuments.find((post) => {
+		const pathAsked =
+			params?.slug && Array.isArray(params.slug)
+				? `/${params.slug.join('/')}`
+				: params?.slug;
+		const pathFound = post.slug;
 
-        return pathAsked === pathFound;
-    });
+		return pathAsked === pathFound;
+	});
 
-    if (document) {
-        return {
-            props: {
-                mdx: document,
-            },
-        };
-    } else {
-        return {
-            props: {
-                error: true,
-            },
-        };
-    }
+	if (document) {
+		return {
+			props: {
+				mdx: document,
+			},
+		};
+	}
+	return {
+		props: {
+			error: true,
+		},
+	};
 };
 
 export const getStaticPaths = async () => {
-    const paths = allDocuments.map((doc) => doc.slug);
+	const paths = allDocuments.map((doc) => doc.slug);
 
-    return {
-        paths,
-        fallback: false,
-    };
+	return {
+		paths,
+		fallback: false,
+	};
 };
