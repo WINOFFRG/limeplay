57b5f2787ce81205561695a05e02c3666b578ef1
winoffrg <61311751+WINOFFRG@users.noreply.github.com>
2023-02-26 02:37:39 +0530

    Added Build Script

--- docs/vercel-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if git diff HEAD^ HEAD --quiet ./docs; then
-    echo "🛑 - No changes in docs folder. Skipping build."
-    exit 0;
-else
-    echo "✅ - Changes detected in docs folder. Triggering build."
-    exit 1;
-fi
