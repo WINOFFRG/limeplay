03f72bf5d0635414307bff1aa4221cfebf9d2670
winoffrg <61311751+WINOFFRG@users.noreply.github.com>
2023-02-26 05:42:10 +0530

    feat:hooks Added Orientation support in Fullscreen Hook

--- packages/limetree/vite.config.ts
+++ packages/limetree/vite.config.ts
@@ -1,20 +1,19 @@
 import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), splitVendorChunkPlugin()],
-    resolve: {
-        alias: {
-            'shaka-player': 'shaka-player/dist/shaka-player.compiled',
-        },
-    },
-    //   optimizeDeps: {
-    //     include: ['shaka-player'],
-    //   },
-    //   build: {
-    //     commonjsOptions: {
-    //       include: [/shaka-player/, /node_modules/],
-    //     },
-    //   },
+	plugins: [
+		react({
+			include: /\.tsx?$/,
+		}),
+		splitVendorChunkPlugin(),
+	],
+	resolve: {
+		alias: {
+			'shaka-player': 'shaka-player/dist/shaka-player.compiled',
+		},
+	},
+	server: {
+		host: '0.0.0.0',
+	},
 });
