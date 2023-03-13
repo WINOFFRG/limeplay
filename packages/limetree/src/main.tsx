3317cf0285876a92b3bed2749938172855bfa9a1
winoffrg <rohangupta1528@gmail.com>
2023-03-13 13:28:21 +0530

    Testing useGesture hook

--- packages/limetree/src/main.tsx
+++ packages/limetree/src/main.tsx
@@ -5,26 +5,12 @@ import { MantineProvider } from '@mantine/styles';
 import FullScreenPlayer from './components/FullScreenPlayer';
 import './globals.css';
 import './fonts/Graphik-Medium.woff';
+import LimeplayProvider from './components/LimeplayProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<MantineProvider
-			withGlobalStyles
-			withNormalizeCSS
-			theme={{
-				colorScheme: 'dark',
-				defaultRadius: '50%',
-
-				fontSizes: {
-					xs: 12,
-					sm: 14,
-					md: 16,
-					lg: 18,
-					xl: 20,
-				},
-			}}
-		>
+		<LimeplayProvider>
 			<FullScreenPlayer />
-		</MantineProvider>
+		</LimeplayProvider>
 	</React.StrictMode>
 );
