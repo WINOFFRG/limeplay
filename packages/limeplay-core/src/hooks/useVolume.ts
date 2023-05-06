f00592e1aed2580fb3ec85b0252ae3f494b490fd
Rohan Gupta <rohangupta1528@gmail.com>
2023-05-07 04:15:42 +0530

    Added Fullscreen Control

--- packages/limeplay-core/src/hooks/useVolume.ts
+++ packages/limeplay-core/src/hooks/useVolume.ts
@@ -83,8 +83,6 @@ export function useVolume({
 			playback.addEventListener(event, volumeEventHandler);
 		});
 
-		console.log('Initial Volume: ', playback.volume);
-
 		playback.volume = playback.muted ? 0 : initialVolume || playback.volume;
 
 		volumeEventHandler();
