35af9e1b34e6ea3c4210a5758dada26d00f83e60
Rohan Gupta <61311751+WINOFFRG@users.noreply.github.com>
2023-05-08 03:44:28 +0530

    Fixed Package.json

--- packages/limeplay-core/src/store/index.ts
+++ packages/limeplay-core/src/store/index.ts
@@ -18,37 +18,36 @@ import {
 export function createLimeplayStore() {
 	const store = createStore<InitialStore & StoreSlice>()(
 		logger(
-			devtools(
-				(set, get, storeApi) => ({
-					playback: null,
-					setPlayback: (playback: HTMLMediaElement) =>
-						set({ playback }),
-					player: null,
-					resetPlayer: () =>
-						set({
-							player: null,
-							playback: null,
-							bufferInfo: null,
-							isLoading: false,
-							isSeeking: false,
-							isLive: false,
-							duration: null,
-							currentTime: null,
-							muted: false,
-						}),
-					setPlayer: (player: shaka.Player) => set({ player }),
-					...createPlaybackSlice(set, get, storeApi),
-					...createLoadingSlice(set, get, storeApi),
-					...createVolumeSlice(set, get, storeApi),
-					...createTimelineSlice(set, get, storeApi),
-					...createBufferSlice(set, get, storeApi),
-					...createSafeLoadSlice(set, get, storeApi),
-					...createFullScreenSlice(set, get, storeApi),
-				}),
-				{
-					name: 'Limeplay Store',
-				}
-			)
+			// devtools(
+			(set, get, storeApi) => ({
+				playback: null,
+				setPlayback: (playback: HTMLMediaElement) => set({ playback }),
+				player: null,
+				resetPlayer: () =>
+					set({
+						player: null,
+						playback: null,
+						bufferInfo: null,
+						isLoading: false,
+						isSeeking: false,
+						isLive: false,
+						duration: null,
+						currentTime: null,
+						muted: false,
+					}),
+				setPlayer: (player: shaka.Player) => set({ player }),
+				...createPlaybackSlice(set, get, storeApi),
+				...createLoadingSlice(set, get, storeApi),
+				...createVolumeSlice(set, get, storeApi),
+				...createTimelineSlice(set, get, storeApi),
+				...createBufferSlice(set, get, storeApi),
+				...createSafeLoadSlice(set, get, storeApi),
+				...createFullScreenSlice(set, get, storeApi),
+			})
+			// 	{
+			// 		name: 'Limeplay Store',
+			// 	}
+			// )
 		)
 	);
 
