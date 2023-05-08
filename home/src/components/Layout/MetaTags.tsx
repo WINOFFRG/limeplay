93615ff7931cd7bb71bfb550881b09fb1448c8c8
Rohan Gupta <rohangupta1528@gmail.com>
2023-05-09 03:13:20 +0530

    Fixed Mobile Responsiveness

--- home/src/components/Layout/MetaTags.tsx
+++ home/src/components/Layout/MetaTags.tsx
@@ -48,7 +48,7 @@ export function MetaTags(props: MetaTagsProps) {
 		children,
 	} = props;
 
-	const title = localTitle || 'Limeplay UI';
+	const title = localTitle || 'Limeplay – Modern Headless UI Library';
 	const image =
 		localImage === false
 			? null
@@ -110,28 +110,33 @@ export function MetaTags(props: MetaTagsProps) {
 
 				{/* Favicons */}
 				<link
-					key="favicon"
 					rel="icon"
-					href="/static/favicon.ico"
-					sizes="any"
+					type="image/x-icon"
+					href="/favicon/favicon.ico"
 				/>
 				<link
-					key="favicon-svg"
 					rel="icon"
-					href={favicon ?? `/static/favicon.svg`}
+					type="image/png"
+					sizes="16x16"
+					href="/favicon/favicon-16x16.png"
 				/>
 				<link
-					key="favicon-touch-icon"
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon/favicon-32x32.png"
+				/>
+				<link
 					rel="apple-touch-icon"
 					sizes="180x180"
-					href="/static/apple-touch-icon.png"
+					href="/favicon/apple-touch-icon.png"
 				/>
 				<link
-					key="favicon-safari-svg"
-					rel="mask-icon"
-					href="/static/limeplay-safari.svg"
-					color="#5E6AD2;"
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/favicon/apple-touch-icon.png"
 				/>
+				<link rel="manifest" href="/favicon/site.webmanifest" />
 
 				{/* URL */}
 				<link key="canonical" rel="canonical" href={BASE_URL + path} />
