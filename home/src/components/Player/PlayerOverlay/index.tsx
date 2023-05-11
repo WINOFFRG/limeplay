d428d533d3ca372193c19b85cd3beacf4c1253d3
Rohan Gupta <rohangupta1528@gmail.com>
2023-05-11 23:27:04 +0530

    Added new CDN Default Link with T stamp in URL

--- home/src/components/Player/PlayerOverlay/index.tsx
+++ home/src/components/Player/PlayerOverlay/index.tsx
@@ -4,6 +4,8 @@ import {
 } from '@limeplay/core/src/store';
 import { useEffect } from 'react';
 import { useSafeLoad } from '@limeplay/core/src/hooks';
+import mux from 'mux.js';
+import { useRouter } from 'next/router';
 import ControlsOverlay from '../ControlsOverlay';
 import useStyles from './styles';
 import PlayerLoader from '../Loader';
@@ -15,11 +17,30 @@ export default function PlayerOverlay() {
 	const player = useLimeplayStore((state) => state.player);
 	const isSafeLoad = useLimeplayStore((state) => state.isSafeLoad);
 	const { getState } = useLimeplayStoreAPI();
+	const router = useRouter();
+
 	const demoPlabackUrl =
-		'https://storage.googleapis.com/nodejs-streaming.appspot.com/uploads/f6b7c492-e78f-4b26-b95f-81ea8ca21a18/1642708128072/manifest.mpd';
+		'https://embed-cloudfront.wistia.com/deliveries/4a77e940176149046375a5036dbf2f7f01ce3a59.m3u8';
+
+	const removeQueryParam = (name) => {
+		const query = { ...router.query };
+		delete query[name];
+		router.push(
+			{
+				pathname: router.pathname,
+				query,
+			},
+			undefined,
+			{ shallow: true }
+		);
+	};
 
 	useEffect(() => {
 		try {
+			if (!window.muxjs) {
+				window.muxjs = mux;
+			}
+
 			if (player && getState().isSafeLoad && player.getLoadMode() === 1) {
 				let playerConfig =
 					process.env.NEXT_PUBLIC_SHAKA_CONFIG ||
@@ -28,18 +49,39 @@ export default function PlayerOverlay() {
 				if (process.env.NEXT_PUBLIC_SHAKA_CONFIG) {
 					playerConfig = JSON.parse(
 						process.env.NEXT_PUBLIC_SHAKA_CONFIG
-					);
+					) as shaka.extern.PlayerConfiguration;
 				}
 
+				playerConfig.abr.enabled = true;
+
 				player.configure(playerConfig);
+
+				const tParam = router.query.t;
+				let startTime = 0;
+
+				if (tParam) {
+					startTime = parseInt(tParam as string, 10);
+				}
+
+				// console.log({ startTime });
+
 				player.load(
-					process.env.NEXT_PUBLIC_PLAYBACK_URL || demoPlabackUrl
+					process.env.NEXT_PUBLIC_PLAYBACK_URL || demoPlabackUrl,
+					startTime
 				);
+
+				// if (startTime > 0) {
+				// 	removeQueryParam('t');
+				// 	playback.play();
+				// }
 			}
 		} catch (error) {
 			console.log(error);
+			throw error;
 		}
-	}, [player, playback, isSafeLoad]);
+	}, [player, playback, isSafeLoad, router]);
+
+	if (!playback) return null;
 
 	return (
 		<div className={classes.overlayWrapper}>
