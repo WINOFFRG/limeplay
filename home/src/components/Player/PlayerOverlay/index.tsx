3f38c7659252f842bc986a34107aa4fe5198d15b
Rohan Gupta <rohangupta1528@gmail.com>
2023-07-12 04:40:39 +0530

    Added Error Handling w/ Shaka Method

--- home/src/components/Player/PlayerOverlay/index.tsx
+++ home/src/components/Player/PlayerOverlay/index.tsx
@@ -2,7 +2,7 @@ import {
 	useLimeplayStore,
 	useLimeplayStoreAPI,
 } from '@limeplay/core/src/store';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSafeLoad } from '@limeplay/core/src/hooks';
 // @ts-ignore
 import mux from 'mux.js';
@@ -20,16 +20,34 @@ export default function PlayerOverlay() {
 	const isSafeLoad = useLimeplayStore((state) => state.isSafeLoad);
 	const { getState } = useLimeplayStoreAPI();
 	const router = useRouter();
+	const [error, setError] = useState<string>(null);
 
 	const demoPlabackUrl =
 		'https://embed-cloudfront.wistia.com/deliveries/4a77e940176149046375a5036dbf2f7f01ce3a59.m3u8';
+
+	if (error) {
+		throw new Error(error);
+	}
 
 	useEffect(() => {
 		if (!window.muxjs) {
 			window.muxjs = mux;
 		}
 
+		const onErrorHandler = (event) => {
+			if (event.code && event.severity) {
+				setError(`
+						Shaka Player failed with an Error Code: ${event.code} :: Severity: ${event.severity}
+					`);
+			} else {
+				setError(`
+						Shaka Player failed with an Error: ${event.message}
+					`);
+			}
+		};
+
 		if (player && getState().isSafeLoad && player.getLoadMode() === 1) {
+			player.addEventListener('error', onErrorHandler);
 			const playerConfig = merge(
 				player.getConfiguration(),
 				JSON.parse(process.env.NEXT_PUBLIC_SHAKA_CONFIG ?? '{}')
@@ -44,11 +62,20 @@ export default function PlayerOverlay() {
 				startTime = parseInt(tParam as string, 10);
 			}
 
-			player.load(
-				process.env.NEXT_PUBLIC_PLAYBACK_URL || demoPlabackUrl,
-				startTime
-			);
+			player
+				.load(
+					process.env.NEXT_PUBLIC_PLAYBACK_URL || demoPlabackUrl,
+					startTime
+				)
+				// Error's during load need to be handled separately
+				.catch(onErrorHandler);
 		}
+
+		return () => {
+			if (player) {
+				player.removeEventListener('error', onErrorHandler);
+			}
+		};
 	}, [player, playback, isSafeLoad, router]);
 
 	if (!playback) return null;
