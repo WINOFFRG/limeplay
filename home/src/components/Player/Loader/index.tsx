5742faf07f85bf23f3f0b63c890c587d21dd6f1b
WINOFFRG <rohangupta1528@gmail.com>
2025-03-08 19:56:45 +0530

    clean: home page app

--- home/src/components/Player/Loader/index.tsx
+++
@@ -1,9 +0,0 @@
-import { useLoading } from '@limeplay/core/src/hooks';
-import useStyles from './styles';
-
-export default function PlayerLoader() {
-	const { classes } = useStyles();
-	const { isLoading } = useLoading();
-
-	return isLoading ? <div className={classes.playerLoader} /> : null;
-}
