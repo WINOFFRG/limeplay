8fdb03c0539f3bb434abaeaeb9ec21a8c6d4fa27
Rohan Gupta <rohangupta1528@gmail.com>
2025-03-23 18:51:41 +0530

    chore: code cleanups

--- docs/settings/types.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Icon2fa } from '@tabler/icons';
-
-export interface Category {
-	title: string;
-	icon?: React.FC;
-	//   icon: (props: React.ComponentProps<typeof Icon2fa>) => JSX.Element;
-}
