85f497efe2e9e5e27fa4840d310bab7450f59b42
Rohan Gupta <146918718+rohangupta-zomato@users.noreply.github.com>
2025-03-23 18:07:32 +0530

    Revert "feat: added docs and base cleanups"

---
+++ home/src/components/Layout/Header.tsx
@@ -0,0 +1,151 @@
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+import Image from 'next/image';
+import { createStyles } from '@mantine/styles';
+import { useCommonStyles } from '@/styles/common';
+import NavigationItem from './Navigation';
+import { GitHubIcon } from '@/assets/icons/GitHubIcon';
+import { DiscordIcon } from '@/assets/icons/DiscordIcon';
+import { Config } from '../../../config';
+import { TwitterIcon } from '@/assets/icons/TwitterIcon';
+import { moreHitArea } from '@/styles/mixins';
+
+export type HeaderProps = {
+	/** Whether the header should be sticky when scrolling. */
+	sticky?: boolean | 'mobile';
+	/** Whether the header should blur content underneath it. */
+	blur?: boolean;
+};
+
+export function Header({ sticky = false, blur = true }: HeaderProps) {
+	const { classes: commonClasses } = useCommonStyles();
+	const { classes } = useStyles({
+		sticky,
+		blur,
+	});
+
+	return (
+		<header className={classes.header}>
+			{blur && <div className={classes.blur} />}
+			<NavigationMenu.Root delayDuration={0} className={classes.menuRoot}>
+				<NavigationMenu.List
+					asChild
+					aria-label="Site navigation"
+					className={classes.menuList}
+				>
+					<ul className={commonClasses.layoutContent}>
+						<div className={classes.brandLogo}>
+							<Image
+								alt="Limeplay Logo"
+								width={25}
+								height={25}
+								src="/brand/logo_white.png"
+							/>
+						</div>
+						<NavigationItem
+							href={Config.DOCS_BASE}
+							variant="dimmed"
+						>
+							Documentation
+						</NavigationItem>
+						<div className={classes.spacer} />
+						<NavigationItem href={Config.GITHUB_URL} type="icon">
+							<GitHubIcon size={24} />
+						</NavigationItem>
+						<NavigationItem href={Config.DISCORD_URL} type="icon">
+							<DiscordIcon size={24} />
+						</NavigationItem>
+						<NavigationItem
+							href={Config.AUTHOR_TWITTER}
+							type="icon"
+						>
+							<TwitterIcon size={24} />
+						</NavigationItem>
+					</ul>
+				</NavigationMenu.List>
+			</NavigationMenu.Root>
+		</header>
+	);
+}
+
+const useStyles = createStyles((theme, { blur, sticky }: HeaderProps) => ({
+	header: {
+		position: sticky ? 'relative' : 'absolute',
+		top: 0,
+		background: !blur ? theme.other.color.pageBg : 'initial',
+		display: 'flex',
+		flexDirection: 'column',
+		left: 0,
+		right: 0,
+		alignItems: 'center',
+		height: 'var(--header-height)',
+		// zIndex: 'var(--layer-header)',
+		transition: 'background 400ms',
+		marginBottom: sticky ? 'initial' : `calc(-1 * var(--header-height))`,
+		marginRight:
+			sticky === 'mobile' || sticky
+				? `var(--removed-body-scroll-bar-size)`
+				: 'initial',
+	},
+
+	blur: {
+		'&::before, &::after': {
+			content: '""',
+			position: 'absolute',
+			inset: 0,
+
+			/* Safari / iOS occasionally have 1px gap at the top of screen, ensure it's blurred */
+			top: '-1px',
+			bottom: '-50%',
+			pointerEvents: 'none',
+			userSelect: 'none',
+		},
+
+		'&::before': {
+			backdropFilter: 'blur(12px)',
+			maskImage:
+				'linear-gradient(to bottom, black var(--header-height), transparent)',
+		},
+	},
+
+	menuRoot: {
+		position: 'relative',
+		width: '100%',
+	},
+
+	menuList: {
+		position: 'relative',
+		display: 'flex',
+		alignItems: 'center',
+
+		'& > li + li': {
+			marginLeft: '24px',
+
+			[theme.fn.smallerThan('lg')]: {
+				marginLeft: '16px',
+			},
+		},
+
+		'&::after': {
+			content: '""',
+			marginLeft: 'var(--page-padding-left)',
+			marginRight: 'var(--page-padding-right)',
+			position: 'absolute',
+			bottom: 0,
+			left: 0,
+			right: 0,
+			height: '1px',
+			background: 'rgba(255, 255, 255, 0.08)',
+		},
+	},
+
+	spacer: {
+		flexGrow: 1,
+	},
+
+	brandLogo: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		padding: '0 12px 0 0',
+	},
+}));
---
+++ home/src/pages/404.tsx
@@ -0,0 +1,41 @@
+import { Text, Title } from '@mantine/core';
+import { Layout } from '@/components/Layout/Layout';
+import { MetaTags } from '@/components/Layout/MetaTags';
+
+export default function Home() {
+	return (
+		<Layout>
+			<MetaTags title="Not found" image={false} noIndex />
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+					justifyContent: 'center',
+					height: '100%',
+					textAlign: 'center',
+					flex: '1 1 0',
+					minHeight: '60vh',
+				}}
+			>
+				<div
+					style={{
+						margin: '0 auto',
+					}}
+				>
+					<svg width="128" fill="#5E6AD2" viewBox="0 0 112 138">
+						<path
+							fillRule="evenodd"
+							clipRule="evenodd"
+							d="M12 4h88a8 8 0 018 8v108H4V12a8 8 0 018-8zM0 12C0 5.373 5.373 0 12 0h88c6.627 0 12 5.373 12 12v112h-8.131v13.156H10V124H0V12zm21.603 5.953h67.954c.565 0 1.023.458 1.023 1.023V72.93c0 .565-.458 1.023-1.023 1.023H21.603a1.023 1.023 0 01-1.023-1.023V18.976c0-.565.458-1.023 1.023-1.023zm-5.023 1.023a5.023 5.023 0 015.023-5.023h67.954a5.023 5.023 0 015.023 5.023V72.93a5.023 5.023 0 01-5.023 5.023H21.603a5.023 5.023 0 01-5.023-5.023V18.976zm83.29 105.522H14v8.658h85.87v-8.658zM92.433 93.94H64.81v5.074h27.626V93.94zm-80.877 10.074h10.046v5.074H11.557v-5.074zm18.836-78.9h-3.767v5.165h3.767v-5.165zm5.023 0h3.767v5.165h-3.767v-5.165zm12.557 0h-3.767v5.165h3.767v-5.165zm5.023 0h3.767v5.165h-3.767v-5.165zm12.557 0h-3.767v5.165h3.767v-5.165zm5.023 0h3.767v5.165h-3.767v-5.165zM30.393 35.16h-3.767v5.164h3.767V35.16zm5.023 0h3.767v5.164h-3.767V35.16zm12.557 0h-3.767v5.164h3.767V35.16zm5.023 0h3.767v5.164h-3.767V35.16zM30.393 45.206h-3.767v5.164h3.767v-5.164zm5.023 0h3.767v5.164h-3.767v-5.164zM30.393 55.25h-3.767v5.165h3.767v-5.164zm5.023 0h3.767v5.165h-3.767v-5.164zm12.557 0h-3.767v5.165h3.767v-5.164zm12.624-6.76l3.114-3.115 7.41 7.41 7.007-7.006 3.293 3.294-7.006 7.007 7.41 7.41-3.114 3.114-7.41-7.41-7.007 7.006-3.293-3.294 7.006-7.006-7.41-7.41z"
+						/>
+					</svg>
+					<Title order={1}>Not found</Title>
+					<Text>
+						The page you&apos;re trying to access does not exist.
+					</Text>
+				</div>
+			</div>
+		</Layout>
+	);
+}
