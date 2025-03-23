49b2a4902051c20c3e13a7d9dbc9f150a7899c95
WINOFFRG <61311751+WINOFFRG@users.noreply.github.com>
2025-03-23 18:29:35 +0530

    feat: setup docs

---
+++ apps/www/components/mdx-components.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from "react"
+import { Callout } from "fumadocs-ui/components/callout"
+import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock"
+import { Tab, Tabs } from "fumadocs-ui/components/tabs"
+import defaultMdxComponents from "fumadocs-ui/mdx"
+import type { MDXComponents } from "mdx/types"
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    ...defaultMdxComponents,
+    ...components,
+    Callout,
+    Suspense,
+    Tab,
+    Tabs,
+    pre: ({ ref: _ref, ...props }) => (
+      <CodeBlock {...props}>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
+  }
+}
