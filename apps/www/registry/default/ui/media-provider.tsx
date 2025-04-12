52cf04133bc62ef4375ee9c88de0bca43de1c0ba
Rohan Gupta <rohangupta1528@gmail.com>
2025-04-12 17:55:18 +0530

    feat: added base ui elements

---
+++ apps/www/registry/default/ui/media-provider.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import React, { createContext, useContext, useRef } from "react"
+import { useStore } from "zustand"
+
+import {
+  createMediaStore,
+  CreateMediaStoreProps,
+  TypeMediaStore,
+} from "@/registry/default/lib/create-media-store"
+
+type MediaProviderContext = ReturnType<typeof createMediaStore>
+
+const MediaProviderContext = createContext<MediaProviderContext | null>(null)
+
+interface MediaProviderProps extends CreateMediaStoreProps {
+  debug?: boolean
+}
+
+export function MediaProvider({
+  children,
+  debug = false,
+  ...props
+}: React.PropsWithChildren<MediaProviderProps>) {
+  const store = useRef(createMediaStore(props)).current
+
+  return (
+    <MediaProviderContext.Provider value={store}>
+      {children}
+    </MediaProviderContext.Provider>
+  )
+}
+
+function useMediaStoreWithContext<T>(
+  selector: (state: TypeMediaStore) => T
+): T {
+  const storeFromCtx = useContext(MediaProviderContext)
+
+  if (!storeFromCtx) {
+    throw new Error("Missing MediaProvider in root")
+  }
+
+  return useStore(storeFromCtx, selector)
+}
+
+let globalStore: ReturnType<typeof createMediaStore> | null = null
+
+// DEV: Migrate to conditional calling without context
+function useMediaStoreWithoutContext<T>(
+  selector: (state: TypeMediaStore) => T,
+  options?: CreateMediaStoreProps
+): T {
+  if (!globalStore) {
+    globalStore = createMediaStore(options || {})
+  }
+
+  return useStore(globalStore, selector)
+}
+
+export const useMediaStore = useMediaStoreWithContext
