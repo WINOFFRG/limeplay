6e9f6127ab4906dfcb57d73fe1e9670a35e49bf8
WINOFFRG <rohangupta1528@gmail.com>
2025-04-12 17:46:45 +0530

    chore: clean root dir and home app

---
+++ apps/www/registry/registry-examples.ts
@@ -0,0 +1,15 @@
+import { type Registry } from "shadcn/registry"
+
+export const examples: Registry["items"] = [
+  {
+    name: "playback-state-control",
+    type: "registry:example",
+    registryDependencies: [""],
+    files: [
+      {
+        path: "examples/playback-state-control.tsx",
+        type: "registry:example",
+      },
+    ],
+  },
+]
