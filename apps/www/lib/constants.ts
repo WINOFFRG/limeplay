15a358a442b30dbfeb0ab8d4777138fca1ec6390
WINOFFRG <61311751+WINOFFRG@users.noreply.github.com>
2025-04-13 21:52:25 +0530

    fix: social links and styles

--- apps/www/lib/constants.ts
+++ apps/www/lib/constants.ts
@@ -1,2 +1,5 @@
 export const PRODUCT_NAME = "Limeplay";
+export const PRODUCT_DECSRIPTION =
+  "A set of beautifully-designed, accessible components for Video Player and a code distribution platform. Works with your favourite frameworks. Open Source. Open Code. Powered by Shaka Player";
 export const GITHUB_URL = "http://git.new/limeplay";
+export const X_URL = "https://dub.sh/winoffrg";
