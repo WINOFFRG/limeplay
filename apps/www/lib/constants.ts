66bd2d76d47b0279a1890cddaf7fcc28e43c3142
WINOFFRG <61311751+WINOFFRG@users.noreply.github.com>
2025-04-19 01:30:48 +0530

    docs: updated project why

--- apps/www/lib/constants.ts
+++ apps/www/lib/constants.ts
@@ -3,3 +3,4 @@ export const PRODUCT_DECSRIPTION =
   "A set of beautifully-designed, accessible components for Video Player and a code distribution platform. Works with your favourite frameworks. Open Source. Open Code. Powered by Shaka Player";
 export const GITHUB_URL = "http://git.new/limeplay";
 export const X_URL = "https://dub.sh/winoffrg";
+export const DISCORD_URL = "https://discord.gg/ZjXFzqmqjn?utm_source=limeplay";
