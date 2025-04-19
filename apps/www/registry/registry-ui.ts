eddcc209eecc890a212a3acbd9cef986cc7f877f
Rohan Gupta <rohan.g@zomato.com>
2025-04-19 20:31:59 +0530

    fix: cleaned build structure

--- apps/www/registry/registry-ui.ts
+++ apps/www/registry/registry-ui.ts
@@ -9,29 +9,34 @@ export const ui: Registry["items"] = [
       {
         path: "ui/mute-control.tsx",
         type: "registry:ui",
+        target: "components/player/mute-control.tsx",
       },
     ],
-    // tailwind: {
-    //   config: {
-    //     theme: {
-    //       extend: {
-    //         keyframes: {
-    //           "accordion-down": {
-    //             from: { height: "0" },
-    //             to: { height: "var(--radix-accordion-content-height)" },
-    //           },
-    //           "accordion-up": {
-    //             from: { height: "var(--radix-accordion-content-height)" },
-    //             to: { height: "0" },
-    //           },
-    //         },
-    //         animation: {
-    //           "accordion-down": "accordion-down 0.2s ease-out",
-    //           "accordion-up": "accordion-up 0.2s ease-out",
-    //         },
-    //       },
-    //     },
-    //   },
-    // },
+  },
+  {
+    name: "media-provider",
+    type: "registry:ui",
+    dependencies: ["zustand"],
+    files: [
+      {
+        path: "ui/mute-control.tsx",
+        type: "registry:ui",
+      },
+    ],
+  },
+  {
+    name: "player-layout",
+    type: "registry:ui",
+    dependencies: ["@radix-ui/react-compose-refs"],
+    files: [
+      {
+        path: "ui/player-layout.tsx",
+        type: "registry:ui",
+      },
+      {
+        path: "ui/media-provider.tsx",
+        type: "registry:ui",
+      },
+    ],
   },
 ];
