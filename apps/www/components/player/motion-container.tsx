554ce32c52eb80f39e24d0c7ac53fc5e330da6f3
Rohan Gupta <61311751+WINOFFRG@users.noreply.github.com>
2025-04-20 03:40:32 +0530

    fix: broken lints

--- apps/www/components/player/motion-container.tsx
+++ apps/www/components/player/motion-container.tsx
@@ -24,7 +24,7 @@ export function MotionContainer({ children }: { children: React.ReactNode }) {
           duration: 0.4,
         },
       }}
-      className="fixed top-1/2 left-1/2 z-100 max-h-[calc(100vh-24px-24px)] w-screen max-w-[calc(100vw-var(--page-padding-left)-var(--page-padding-right))]"
+      className="z-100 fixed left-1/2 top-1/2 max-h-[calc(100vh-24px-24px)] w-screen max-w-[calc(100vw-var(--page-padding-left)-var(--page-padding-right))]"
     >
       {children}
     </motion.div>
