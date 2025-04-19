540137c725c3b3f126a4d2fd0db78ab5b9fa2eda
Rohan Gupta <rohan.g@zomato.com>
2025-04-20 02:44:26 +0530

    feat: added vercel speed insights

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -6,6 +6,7 @@ import { Metadata } from "next";
 import { PRODUCT_DECSRIPTION, PRODUCT_NAME } from "@/lib/constants";
 import { ThemeProvider } from "next-themes";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"]
@@ -24,6 +25,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="antialiased">
         <Analytics />
+        <SpeedInsights />
         <ThemeProvider
           storageKey="limeplay-ui-theme"
           defaultTheme="dark"
