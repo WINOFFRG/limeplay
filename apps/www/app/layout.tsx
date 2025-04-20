4b8884079d6d04233b976ae896a5d07aa8ba8f96
Rohan Gupta <rohangupta1528@gmail.com>
2025-04-21 00:14:49 +0530

    fix: updated branding and docs

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -6,7 +6,11 @@ import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { ThemeProvider } from "next-themes"
 
-import { PRODUCT_DECSRIPTION, PRODUCT_NAME } from "@/lib/constants"
+import {
+  PRODUCT_DECSRIPTION,
+  PRODUCT_NAME,
+  PROD_BASE_HOST,
+} from "@/lib/constants"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -17,7 +21,48 @@ export const metadata: Metadata = {
     default: PRODUCT_NAME,
     template: `%s | ${PRODUCT_NAME}`,
   },
+  metadataBase: new URL(PROD_BASE_HOST),
   description: PRODUCT_DECSRIPTION,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "shaka-player",
+    "shaka player",
+    "video player",
+    "media",
+    "ui",
+  ],
+  authors: [
+    {
+      name: "winoffrg",
+      url: "https://github.com/winoffrg",
+    },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: PROD_BASE_HOST,
+    title: PRODUCT_NAME,
+    description: PRODUCT_DECSRIPTION,
+    siteName: PRODUCT_NAME,
+    images: [
+      {
+        url: `${PROD_BASE_HOST}/opengraph-image.png`,
+        width: 1200,
+        height: 630,
+        alt: PRODUCT_NAME,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: PRODUCT_NAME,
+    description: PRODUCT_DECSRIPTION,
+    images: [`${PROD_BASE_HOST}/opengraph-image.png`],
+    creator: "@shadcn",
+  },
+  manifest: `${PROD_BASE_HOST}/site.webmanifest`,
 }
 
 export default function Layout({ children }: { children: ReactNode }) {
