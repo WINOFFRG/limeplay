23f31b75486f121cdf678e64bcd588885fb64d94
winoffrg <rohangupta1528@gmail.com>
2025-08-07 00:11:49 +0530

    fix: added missing dependencies and registry code

--- apps/www/registry/default/lib/utils.ts
+++ apps/www/registry/default/lib/utils.ts
@@ -1,4 +1,10 @@
 import type React from "react"
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 /**
  * Type-safe event handler utility function
