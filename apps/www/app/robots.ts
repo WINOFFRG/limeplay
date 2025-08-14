243892248d05250645c6ecd153f036e892cdc0cd
winoffrg <61311751+WINOFFRG@users.noreply.github.com>
2025-08-15 02:26:01 +0530

    fix: build errors

--- apps/www/app/robots.ts
+++ apps/www/app/robots.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next"
 
 import { PROD_BASE_HOST } from "@/lib/constants"
 
+export const dynamic = "force-static"
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
