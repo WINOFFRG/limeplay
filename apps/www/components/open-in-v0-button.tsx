b8e9c43c3490487dd40e6d9b3ec1f0f6beeb5e40
winoffrg <rohangupta1528@gmail.com>
2025-08-18 20:11:12 +0530

    feat: added basic player to get started

--- apps/www/components/open-in-v0-button.tsx
+++ apps/www/components/open-in-v0-button.tsx
@@ -21,8 +21,9 @@ export function OpenInV0Button({
       {...props}
     >
       <a
-        href={`https://v0.dev/chat/api/open?url=${PROD_BASE_HOST}/r/styles/${V0_STYLE}/${name}.json`}
+        href={`https://v0.dev/chat/api/open?url=${encodeURIComponent(`${PROD_BASE_HOST}/r/styles/${V0_STYLE}/${name}.json`)}`}
         target="_blank"
+        rel="noopener noreferrer"
       >
         Open in <Icons.v0 className="size-5" />
       </a>
--- apps/www/registry/default/blocks/basic-player/media-player.tsx
+++ apps/www/registry/default/blocks/basic-player/media-player.tsx
@@ -1,32 +1,39 @@
 import React from "react"
 
 import { cn } from "@/lib/utils"
-import { BottomControls } from "@/registry/default/blocks/linear-player/components/bottom-controls"
-import { MediaElement } from "@/registry/default/blocks/linear-player/components/media-element"
-import { PlayerHooks } from "@/registry/default/blocks/linear-player/components/player-hooks"
+import { MediaElement } from "@/registry/default/blocks/basic-player/components/media-element"
+import { PlaybackStateControl } from "@/registry/default/blocks/basic-player/components/playback-state-control"
+import { PlayerHooks } from "@/registry/default/blocks/basic-player/components/player-hooks"
 import { FallbackPoster } from "@/registry/default/ui/fallback-poster"
 import { LimeplayLogo } from "@/registry/default/ui/limeplay-logo"
 import { MediaProvider } from "@/registry/default/ui/media-provider"
 import * as Layout from "@/registry/default/ui/player-layout"
 import { RootContainer } from "@/registry/default/ui/root-container"
 
-export interface LinearMediaPlayerProps {
-  src: string
+export interface BasicMediaPlayerProps
+  extends React.ComponentPropsWithoutRef<"div"> {
+  src?: string
   debug?: boolean
   className?: string
 }
 
-export const LinearMediaPlayer = React.forwardRef<
+const DEFAULT_SRC =
+  "https://ad391cc0d55b44c6a86d232548adc225.mediatailor.us-east-1.amazonaws.com/v1/master/d02fedbbc5a68596164208dd24e9b48aa60dadc7/singssai/master.m3u8"
+
+export const LimeplayMediaPlayer = React.forwardRef<
   HTMLDivElement,
-  LinearMediaPlayerProps
->(({ src, debug = false, className }, ref) => {
+  BasicMediaPlayerProps
+>((props, ref) => {
+  const { className, src = DEFAULT_SRC, ...etc } = props
+
   return (
-    <MediaProvider debug={debug}>
+    <MediaProvider>
       <RootContainer
         ref={ref}
         height={720}
         width={1280}
         className={cn("m-auto w-full md:min-w-80", className)}
+        {...etc}
       >
         <Layout.PlayerContainer>
           <FallbackPoster className="bg-black">
@@ -37,7 +44,7 @@ export const LinearMediaPlayer = React.forwardRef<
           <Layout.ControlsOverlayContainer />
           <Layout.ControlsContainer>
             <Layout.ControlsBottomContainer>
-              <BottomControls />
+              <PlaybackStateControl />
             </Layout.ControlsBottomContainer>
           </Layout.ControlsContainer>
         </Layout.PlayerContainer>
@@ -46,4 +53,4 @@ export const LinearMediaPlayer = React.forwardRef<
   )
 })
 
-LinearMediaPlayer.displayName = "LinearMediaPlayer"
+LimeplayMediaPlayer.displayName = "LimeplayMediaPlayer"
