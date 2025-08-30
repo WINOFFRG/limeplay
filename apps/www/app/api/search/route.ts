6b0f6747dca6eb1e9117c86800be1ac551a9c6f7
winoffrg <rohangupta1528@gmail.com>
2025-08-31 01:55:20 +0530

    chore: project cleanups

--- apps/www/app/api/search/route.ts
+++
@@ -1,7 +0,0 @@
-import { createFromSource } from "fumadocs-core/search/server"
-
-import { source } from "@/lib/source"
-
-export const revalidate = false
-
-export const { staticGET: GET } = createFromSource(source)
