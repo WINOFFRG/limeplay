211bc8c205a10a061c1a058f8d5d024872710e47
winoffrg <rohangupta1528@gmail.com>
2025-10-24 03:11:19 +0530

    chore: bump nextjs and fumadocs version to major

--- apps/www/app/docs/layout.tsx
+++ apps/www/app/docs/layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react"
 import { DocsLayout } from "fumadocs-ui/layouts/docs"
-import { RootProvider } from "fumadocs-ui/provider"
+import { RootProvider } from "fumadocs-ui/provider/next"
 
 import { source } from "@/lib/source"
 import { baseOptions } from "@/app/layout.config"
