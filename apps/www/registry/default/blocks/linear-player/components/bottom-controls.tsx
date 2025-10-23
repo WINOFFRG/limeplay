dcb5b0dafbe28da8a3cd661ed7849e7081f70461
winoffrg <rohangupta1528@gmail.com>
2025-10-23 13:16:17 +0530

    fix: volume group name

--- apps/www/registry/default/blocks/linear-player/components/bottom-controls.tsx
+++ apps/www/registry/default/blocks/linear-player/components/bottom-controls.tsx
@@ -2,15 +2,15 @@ import { CaptionsStateControl } from "@/registry/default/blocks/linear-player/co
 import { PlaybackRateControl } from "@/registry/default/blocks/linear-player/components/playback-rate-control"
 import { PlaybackStateControl } from "@/registry/default/blocks/linear-player/components/playback-state-control"
 import { Playlist } from "@/registry/default/blocks/linear-player/components/playlist"
-import { TimelineSliderControl } from "@/registry/default/blocks/linear-player/components/timeline-slider-control"
-import { VolumeControl } from "@/registry/default/blocks/linear-player/components/volume-control"
+import { TimelineSliderControl } from "@/registry/default/blocks/linear-player/components/timeline-slider-control";
+import { VolumeGroupControl } from "@/registry/default/blocks/linear-player/components/volume-group-control"
 import * as Layout from "@/registry/default/ui/player-layout"
 
 export function BottomControls() {
   return (
     <Layout.ControlsBottomContainer>
       <PlaybackStateControl />
-      <VolumeControl />
+      <VolumeGroupControl />
       <TimelineSliderControl />
       <PlaybackRateControl />
       <CaptionsStateControl />
