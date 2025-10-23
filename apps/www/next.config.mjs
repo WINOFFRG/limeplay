c9c7b2474704407b8240ae53e07613d315644c90
winoffrg <61311751+WINOFFRG@users.noreply.github.com>
2025-10-24 03:11:19 +0530

    chore: bump nextjs and fumadocs version to major

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -1,15 +1,16 @@
-import type { NextConfig } from "next"
 import { createMDX } from "fumadocs-mdx/next"
 
 const withMDX = createMDX()
 
-const config: NextConfig = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const config = {
   reactStrictMode: true,
   output: "export",
   images: {
     unoptimized: true,
   },
-  productionBrowserSourceMaps: process.env.VERCEL_ENV !== "production",
 }
 
 export default withMDX(config)
