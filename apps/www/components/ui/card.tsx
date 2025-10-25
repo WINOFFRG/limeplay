2b2e6717ab6fba57473ba7464145a51aa75f9cea
winoffrg <rohangupta1528@gmail.com>
2025-10-26 03:29:28 +0530

    chore: linting and format

--- apps/www/components/ui/card.tsx
+++ apps/www/components/ui/card.tsx
@@ -79,10 +79,13 @@ function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="card-footer"
-      className={cn(`
-        flex items-center px-6
-        [.border-t]:pt-6
-      `, className)}
+      className={cn(
+        `
+          flex items-center px-6
+          [.border-t]:pt-6
+        `,
+        className
+      )}
       {...props}
     />
   )
