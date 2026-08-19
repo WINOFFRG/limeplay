bf14922a1a676106a1be7a966b7f243b7f17093d
winoffrg <61311751+WINOFFRG@users.noreply.github.com>
2026-08-19 17:26:31 +0530

    chore: code cleaups

--- apps/www/registry/collection/registry-ui.ts
+++ apps/www/registry/collection/registry-ui.ts
@@ -54,7 +54,7 @@ export const ui: Registry["items"] = [
         "lp-primary-foreground": "oklch(0.985 0 0)",
       },
     },
-    dependencies: ["zustand"],
+    dependencies: ["zustand", "immer"],
     files: [
       {
         path: "ui/media-provider.tsx",
